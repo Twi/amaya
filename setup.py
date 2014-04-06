f743220b6d6868b3a489a1843dda329ed0a7d5c4
Nicole Brennan <twipony.ts@gmail.com>
2014-04-06 14:52:43 -0700

    Add Python setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='amaya',
+      version='0.1',
+      description='IRCv3 capable bot framework',
+      url='http://github.com/bookhorse/amaya',
+      author='Nicole Brennan',
+      author_email='twipony.ts@gmail.com',
+      license='ZLib',
+      packages=['amaya'])
